c511184b98d35553e30602ce6727b8004e6e4599
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-13 22:46:41 +0100

    style(footer): created basic footer

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,5 +2,6 @@
   <div>
     <CommonNavbarComponent />
     <NuxtPage />
+    <CommonFooterComponent />
   </div>
 </template>
---
+++ frontend/components/common/FooterComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-16 bg-dark-main flex justify-center fixed bottom-0 z-50 border-t border-dark-primary">
+        <div class="w-5/6 max-w-[80rem] h-full"></div>
+    </div>
+</template>
