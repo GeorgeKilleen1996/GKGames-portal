3cff645bc2ee86e6ad3dcccd1db5d3ffb5f78458
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-13 22:49:39 +0100

    content(footer): + date, copyright & my name

--- frontend/components/common/FooterComponent.vue
+++ frontend/components/common/FooterComponent.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="w-full h-16 bg-dark-main flex justify-center fixed bottom-0 z-50 border-t border-dark-primary">
-        <div class="w-5/6 max-w-[80rem] h-full"></div>
+        <div class="w-5/6 max-w-[80rem] h-full flex items-center justify-center text-center">
+            <div class="text-xs text-dark-highlight tracking-tightest">
+                {{ new Date().getFullYear() }} &copy; George Killeen. All rights reserved.
+            </div>
+        </div>
     </div>
 </template>
