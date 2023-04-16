5583175d78848f79e1514e8c32a04a631a923f80
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-16 15:27:22 +0100

    style(index): removed password to use magic links

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,8 @@
+
 <template>
     <div class="w-full h-screen flex justify-center items-center bg-dark-main relative overflow-hidden">
-        <CommonMovingBlobComponent class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[25rem] w-[20rem] h-[17.5rem] md:h-[25rem]"/>
-        <div class="p-10 py-14 rounded-lg bg-dark-tertiary flex flex-col md:w-[25rem] w-[22.5rem] shadow-sm border border-dark-card z-10 h-[33rem]">
+        <CommonMovingBlobComponent class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[25rem] w-[20rem] h-[17.5rem] md:h-[15rem]"/>
+        <div class="p-10 py-14 rounded-lg bg-dark-tertiary flex flex-col md:w-[25rem] w-[22.5rem] shadow-sm border border-dark-card z-10 h-[25rem]">
             <!-- Card Header -->
             <div class="w-full h-14 flex justify-start items-center">
                 <!-- Icon -->
@@ -17,17 +18,17 @@
             <!-- Card Body -->
             <div class="w-full mt-8">
                 <form>
-                    <div class="w-full grid grid-rows-3 grid-cols-1 gap-4">
+                    <div class="w-full grid grid-rows-2 grid-cols-1 gap-4">
                         <!-- Email -->
                         <div class="w-full">
                             <label for="" class="text-dark-highlight tracking-tightest text-sm font-light">Email Address</label>
                             <input type="email" class="w-full rounded bg-dark-secondary h-12 px-2 border border-dark-primary mt-2 text-white">
                         </div>
                         <!-- Password -->
-                        <div class="w-full">
+                        <!-- <div class="w-full">
                             <label for="" class="text-dark-highlight tracking-tight text-sm font-light">Your Password</label>
                             <input type="password" class="w-full rounded bg-dark-secondary h-12 px-2 border border-dark-primary mt-2 text-white">
-                        </div>
+                        </div> -->
                         <!-- Sign In -->
                         <div class="w-full">
                             <button class="w-full h-12 rounded bg-dark-primary text-white text-sm tracking-wide mt-2">Sign In</button>
