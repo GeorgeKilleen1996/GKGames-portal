03231d5a2fb5a2c45b00c4d245e1b0e4502c6e68
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-16 18:11:49 +0100

    feat(signin): user redirected once magic link used

--- frontend/pages/auth/signin.vue
+++ frontend/pages/auth/signin.vue
@@ -1,6 +1,17 @@
+<script setup>
+const router = useRouter();
+const client = useSupabaseClient();
+const user = useSupabaseUser();
+
+watchEffect(() => {
+    if (user.value) {
+        router.push('/dashboard')
+    }
+})
+</script>
 <template>
     <div class="w-full h-screen flex justify-center items-center bg-dark-main relative overflow-hidden">
-        <CommonMovingBlobComponent class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[15rem] w-[15rem] h-[15rem] md:h-[15rem]"/>
+        <CommonMovingBlobComponent class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[15rem] w-[15rem] h-[15rem] md:h-[15rem]" />
         <div class="p-10 py-14 rounded-lg bg-dark-tertiary flex flex-col md:w-[25rem] w-[22.5rem] shadow-sm border border-dark-card z-10 h-[17.5rem]">
             <!-- Card Header -->
             <div class="w-full h-14 flex justify-center items-center">
