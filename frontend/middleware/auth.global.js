434a938a711a42f1b6292614d6f1e5feacda6e94
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 12:55:11 +0100

    reset: auth middleware

--- frontend/middleware/auth.global.js
+++ frontend/middleware/auth.global.js
@@ -8,31 +8,4 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const pathSuffix = to.fullPath.split('/')[1];
 
-    if(!user.value){ // If the user is not logged in
-        if(pathSuffix === 'admin' || pathSuffix === 'user'){
-            router.push('/');
-        }
-    } else { // If the user is logged in
-        supabase.from('user').select('permission').eq('id', user.value.id).then(({data, error}) => {
-            if(data.length === 0){
-                // If the user is not in the database create a user...
-                supabase.from('user').insert([
-                    {
-                        id: user.value.id,
-                        permission: 0
-                    }
-                ]).then(({data, error}) => {
-                    if(error){
-                        console.log(error);
-                    } else {
-                        router.push('/user/account'); //Redirect to user section
-                    }
-                });
-            } else if(data[0].permission > 0){
-                router.push('/admin/'); //Redirect to admin section
-            } else {
-                router.push('/user/'); //Redirect to user section
-            }
-        });
-    }
 })
