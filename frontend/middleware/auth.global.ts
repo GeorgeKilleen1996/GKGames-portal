8c0db81a894b1e7b90b6eacc49abe8859ec215ac
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 15:51:28 +0100

    clean(middleware): removed more unused code & cleaned it up

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,19 +1,13 @@
-// Checking whether the user is logged in or not
 export default defineNuxtRouteMiddleware((to, from) => {
 
-    //Getting the supabase user...
     const user = useSupabaseUser();
-    const supabase = useSupabaseClient();
-
     const pathSuffix = to.fullPath.split('/')[1];
 
     if(!user.value){
-        //console.log('not logged in')
         if(pathSuffix == '' || pathSuffix != 'auth'){
             return navigateTo('/auth/login');
         }
     } else {
-        //console.log('logged in')
         if(pathSuffix == '' || pathSuffix == 'auth'){
             return navigateTo('/admin/');
         }
