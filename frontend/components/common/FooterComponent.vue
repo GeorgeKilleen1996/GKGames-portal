bda779c00efebd5c7983aac492ecca0b86839c56
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 18:23:58 +0100

    style(footer): removed footer when user is logged in

--- frontend/components/common/FooterComponent.vue
+++ frontend/components/common/FooterComponent.vue
@@ -1,3 +1,12 @@
+<script setup>
+const user = useSupabaseUser();
+
+const isLoggedIn = ref(false);
+
+watchEffect(() => {
+    isLoggedIn.value = (!user.value) ? false : true;
+})
+</script>
 <template>
     <div class="w-full h-12 bg-dark-main flex justify-center fixed bottom-0 z-50 border-t border-dark-primary">
         <div class="w-5/6 max-w-[80rem] h-full flex items-center justify-center text-center">
