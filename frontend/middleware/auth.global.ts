25ede03788262e3d81390de12d83b52374765329
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 15:37:10 +0100

    feat(middleware): thinned out the code

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -4,10 +4,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
     //Getting the supabase user...
     const user = useSupabaseUser();
     const supabase = useSupabaseClient();
-    const router = useRouter();
 
     const pathSuffix = to.fullPath.split('/')[1];
-    console.log(user.value);
 
     if(!user.value){
         //console.log('not logged in')
