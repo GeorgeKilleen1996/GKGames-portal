9d9c15a57b58872aefdb908137a63b7bafd6d0fb
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 18:27:06 +0100

    style(footer): applied v-if loggedin to footer

--- frontend/components/common/FooterComponent.vue
+++ frontend/components/common/FooterComponent.vue
@@ -8,7 +8,7 @@ watchEffect(() => {
 })
 </script>
 <template>
-    <div class="w-full h-12 bg-dark-main flex justify-center fixed bottom-0 z-50 border-t border-dark-primary">
+    <div class="w-full h-12 bg-dark-main flex justify-center fixed bottom-0 z-50 border-t border-dark-primary" v-if="!isLoggedIn">
         <div class="w-5/6 max-w-[80rem] h-full flex items-center justify-center text-center">
             <div class="text-xs text-dark-highlight tracking-tightest opacity-50">
                 {{ new Date().getFullYear() }} &copy; George Killeen. All rights reserved.
