252b6b4fb3f4da483c27658f87b369b8567ddf0b
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-18 18:04:19 +0100

    style(navbar): reduced navbar size & altered logo colours

--- frontend/components/common/NavbarComponent.vue
+++ frontend/components/common/NavbarComponent.vue
@@ -25,23 +25,23 @@ const logOut = async () => {
 
 </script>
 <template>
-    <div class="w-full h-16 bg-dark-main flex justify-center fixed top-0 z-50 border-b border-dark-primary">
+    <div class="w-full h-14 bg-dark-main flex justify-center fixed top-0 z-50 border-b border-dark-primary">
         <div class="w-5/6 max-w-[80rem] h-full">
             <div class="w-full h-full flex items-center justify-between">
                 <!-- Logo -->
                 <div class="w-16 h-16 relative flex justify-center items-center">
-                    <div class="w-12 h-12 rounded-full bg-dark-primary opacity-10 absolute"></div>
-                    <div class="tracking-widest font-semibold text-dark-primary text-lg"><i class="fas fa-bolt fa-lg"></i></div>
+                    <div class="w-10 h-10 rounded-full bg-gradient-to-br from-cyan-400 to-pink-600 opacity-10 absolute"></div>
+                    <div class="tracking-widest font-semibold text-transparent bg-clip-text bg-gradient-to-br from-cyan-400 to-pink-600 text-md"><i class="fas fa-bolt fa-lg"></i></div>
                 </div>
                 <!-- Links -->
                 <div class="w-full h-full flex items-center justify-center">
                     <div class="w-full h-full flex items-center justify-center gap-4">
                     </div>
                 </div>
-                <!-- Sign In -->
-                <div class="w-full h-full flex items-center justify-end">
-                    <NuxtLink class="text-sm text-dark-highlight cursor-pointer hover:text-[#E94560] transition-all" v-if="!isLoggedIn">Contact Us</NuxtLink>
-                    <NuxtLink class="text-sm text-dark-highlight cursor-pointer hover:text-[#E94560] transition-all" v-else @click="logOut()">Sign Out</NuxtLink>
+                <!-- Logged In Components -->
+                <div class="w-full h-full flex items-center justify-end" v-if="isLoggedIn">
+                    <!-- <NuxtLink class="text-sm text-dark-highlight cursor-pointer hover:text-[#E94560] transition-all" v-if="!isLoggedIn">Contact Us</NuxtLink> -->
+                    <NuxtLink class="text-sm text-dark-highlight cursor-pointer hover:text-[#E94560] transition-all" @click="logOut()">Sign Out</NuxtLink>
                 </div>
             </div>
         </div>
