321673d31705e8083b7b445b7101cb083bbde7b1
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-23 20:07:24 +0100

    style(dashboard): + stats cards titles w/ accurate icons

--- frontend/components/dashboard/StatisticsCardComponent.vue
+++ frontend/components/dashboard/StatisticsCardComponent.vue
@@ -1,8 +1,28 @@
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: String,
+        required: true
+    },
+});
+</script>
 <template>
     <div class="w-full h-full bg-dark-main rounded-lg border border-dark-primary drop-shadow-md relative p-2">
         <!-- Icon -->
         <div class="w-10 h-10 rounded-lg absolute top-2 right-2 bg-zinc-800 flex justify-center items-center">
-            <i class="text-dark-primary fas fa-chart-line"></i>
+            <i class="text-dark-primary" :class="props.icon"></i>
+        </div>
+        <!-- Card Title -->
+        <div class="h-10 rounded-lg flex justify-start items-center px-2 text-dark-highlight">
+            {{ props.title }}
         </div>
     </div>
 </template>
--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-full h-screen flex flex-col justify-start items-center bg-dark-main relative overflow-hidden">
         <div class="w-full h-48 grid lg:grid-cols-4 lg:grid-rows-1 grid-cols-2 grid-rows-2 gap-4">
-            <DashboardStatisticsCardComponent />
-            <DashboardStatisticsCardComponent />
-            <DashboardStatisticsCardComponent />
-            <DashboardStatisticsCardComponent />
+            <DashboardStatisticsCardComponent icon="fas fa-globe" title="GK Games Sales" data="website" />
+            <DashboardStatisticsCardComponent icon="fab fa-ebay" title="eBay Sales"  data="ebay"/>
+            <DashboardStatisticsCardComponent icon="fab fa-facebook" title="FB Marketplace Sales" data="facebook" />
+            <DashboardStatisticsCardComponent icon="fas fa-sack-dollar" title="Total Sales" data="total" />
         </div>
         <div class="w-full h-[calc(100vh-18.5rem)] grid grid-cols-2 grid-rows-1 gap-4 mt-4">
             <DashboardOrdersCardComponent />
