bd019ad8147079f5b78d75ad55a73818b08561aa
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-23 15:29:23 +0100

    style(dashboard): + orders & activity cards

---
+++ frontend/components/dashboard/ActivityCardComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="w-full h-full bg-dark-main rounded-lg border border-dark-primary drop-shadow-md relative p-2">
+        <!-- Icon -->
+        <div class="w-10 h-10 rounded-lg absolute top-2 right-2 bg-zinc-800 flex justify-center items-center">
+            <i class="text-dark-primary fas fa-chart-line"></i>
+        </div>
+    </div>
+</template>
--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="w-full h-screen flex justify-center items-start bg-dark-main relative overflow-hidden">
+    <div class="w-full h-screen flex flex-col justify-start items-center bg-dark-main relative overflow-hidden">
         <div class="w-full h-48 grid lg:grid-cols-4 lg:grid-rows-1 grid-cols-2 grid-rows-2 gap-4">
             <DashboardStatisticsCardComponent />
             <DashboardStatisticsCardComponent />
             <DashboardStatisticsCardComponent />
             <DashboardStatisticsCardComponent />
+        </div>
+        <div class="w-full h-[calc(100vh-18.5rem)] grid grid-cols-2 grid-rows-1 gap-4 mt-4">
+            <DashboardOrdersCardComponent />
+            <DashboardActivityCardComponent />
         </div>
     </div>
 </template>
