818740e0ed0bd76be6e8a1a3cf75f079917d56ae
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-04-23 15:12:05 +0100

    style(dashboard): created statistics card for dashboard

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="w-full h-screen flex justify-center items-center bg-dark-main relative overflow-hidden">
-
+    <div class="w-full h-screen flex justify-center items-start bg-dark-main relative overflow-hidden">
+        <div class="w-full h-48 grid lg:grid-cols-4 lg:grid-rows-1 grid-cols-2 grid-rows-2 gap-4">
+            <DashboardStatisticsCardComponent />
+            <DashboardStatisticsCardComponent />
+            <DashboardStatisticsCardComponent />
+            <DashboardStatisticsCardComponent />
+        </div>
     </div>
 </template>
